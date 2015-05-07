221211197f6774c647e5649cf859d7479d617bf7
David Noble <dnoble@splunk.com>
2015-05-07 14:12:24 -0700

    Adding chunked_searchcommands package

---
+++ examples/chunked_searchcommands/package/bin/_pydebug_conf.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+#
+# Copyright © Splunk, Inc. All Rights Reserved
+
+from __future__ import unicode_literals
+
+host = 'python-debug-server'
+port = 52001
+suspend = False
+is_enabled = True
